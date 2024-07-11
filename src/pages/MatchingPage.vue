cbdf5e1ef191dd1e24418ad1f0ed96874c784c4e
shin-fued <wachirawit.yea@student.mahidol.edu>
2024-07-12 02:02:59 +0700

    like button needs styling'

--- src/pages/MatchingPage.vue
+++ src/pages/MatchingPage.vue
@@ -2,7 +2,7 @@
   <div class="matching">
     <h1>Welcome to the Matching Page</h1>
     <router-link to="/edit-profile" class="button">Edit Profile</router-link>
-    <div v-if="users.length">
+    <!--<div v-if="users.length">
       <div v-for="user in users" :key="user.id" class="user">
         <router-link :to="{ name: 'Matches', params: { id: user.id }}">
           <h2>
@@ -13,8 +13,10 @@
     </div>
     <div v-else>
       Loading Page
-    </div>
+    </div>-->
   </div>
+  <button style="font-size:24px" @click="swipeRight()">Like <i class="fa fa-heart"></i></button>
+  
 
 </template>
 
@@ -22,24 +24,39 @@
 
 
 <script>
-import { useRouter } from 'vue-router';
-import apiClient from "@/axiosConfig";
-import router from "@/router";
-
+import {useRouter} from 'vue-router';
 
 export default {
   name: 'Matching',
   data(){
     return{
+      user: "",
+      noped: new Set(),
+      matched: new Set(),
       users: [
       ]
     }
   },
-  mounted(){
-    fetch('http://localhost:8000/users')
-        .then(res => res.json())
-        .then(data => this.users = data)
-        .catch(err => console.log(err.message))
+  async created(){
+    await this.nextUser()
+  },
+  methods:{
+    async nextUser(){
+      this.user = await fetch('baseURL/users') //actually connect it to db and check for format
+          .then(res => res.json())
+          .then(data => this.users = data)
+          .catch(err => console.log(err.message));
+  },
+    async swipeRight(){
+      this.matched.push(this.user)
+      //send it database as well
+      await this.nextUser()
+    },
+    async swipeLeft(){
+      this.noped.push(this.user)
+      //send it database as well, also double check how to handle so we dont get duplicate users
+      await this.nextUser()
+    },
   },
   setup() {
     const router = useRouter();
@@ -80,6 +97,12 @@ export default {
   text-decoration: none;
   border-radius: 5px;
   text-align: center;
+}
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
 }
 
 </style>
