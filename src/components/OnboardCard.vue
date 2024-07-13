6aa6a454b2c7d5c3c0f6995482d9db0e0eb33c15
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 04:54:14 +0700

    Updated OnBoarding Card Component

---
+++ src/components/OnboardCard.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-card
+    color="primary"
+    variant="flat"
+  >
+    <v-card-title>
+      <h1>FALL INTO HATE</h1>
+      <h1>HATE INTO LOVE</h1>
+     <div class="text-caption d-flex ga-1">
+       Powered by the<div class="d-block"><span class="font-weight-bold"> Hate</span><span class="font-weight-light">Matcher</span>™</div>  world leading, patent pending technology.
+     </div>
+    </v-card-title>
+  </v-card>
+</template>
+
+<style scoped>
+
+</style>
