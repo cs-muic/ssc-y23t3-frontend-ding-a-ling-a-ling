ed0ddcc79f148ff1721cf4c0a509d52d9687fd34
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-13 23:12:59 +0700

    Added placeholder preloader

---
+++ src/components/Preloader.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="preloader">
+    <v-progress-circular color="primary" indeterminate />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'Preloader',
+  })
+</script>
+
+<style scoped>
+.preloader {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+</style>
