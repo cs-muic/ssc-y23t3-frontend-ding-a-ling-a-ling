e8f5c154f91ae2dcd494e0cc74221afd5e92ab6a
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 04:56:22 +0700

    Added Theme

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -9,11 +9,53 @@ import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
 // Composables
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
+
+const HateMatchTheme: ThemeDefinition = {
+  dark: false,
+  colors: {
+    background: '#FFFFFF',
+    surface: '#FFFFFF',
+    'surface-bright': '#FFFFFF',
+    'surface-light': '#FFFFFF',
+    'surface-variant': '#424242',
+    'on-surface-variant': '#FFFFFF',
+    primary: '#DC2D2D',
+    'primary-darken-1': '#1F5592',
+    secondary: '#2B201A',
+    'secondary-darken-1': '#018786',
+    error: '#B00020',
+    info: '#2196F3',
+    success: '#4CAF50',
+    warning: '#FB8C00',
+  },
+  variables: {
+    'border-color': '#2B201A',
+    'border-opacity': 0.12,
+    'high-emphasis-opacity': 0.87,
+    'medium-emphasis-opacity': 0.60,
+    'disabled-opacity': 0.38,
+    'idle-opacity': 0.04,
+    'hover-opacity': 0.04,
+    'focus-opacity': 0.12,
+    'selected-opacity': 0.08,
+    'activated-opacity': 0.12,
+    'pressed-opacity': 0.12,
+    'dragged-opacity': 0.08,
+    'theme-kbd': '#212529',
+    'theme-on-kbd': '#FFFFFF',
+    'theme-code': '#F5F5F5',
+    'theme-on-code': '#000000',
+    'v-card-border-radius': 100,
+  },
+}
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
-    defaultTheme: 'light',
+    defaultTheme: 'loveHateTheme',
+    themes: {
+      loveHateTheme: HateMatchTheme,
+    },
   },
 })
