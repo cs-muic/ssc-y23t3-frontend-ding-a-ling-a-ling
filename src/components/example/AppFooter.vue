6270a3f0e9e67660e0b3a54c4a4b0aa0e19611be
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 06:59:59 +0700

    Styled Footer

--- src/components/example/AppFooter.vue
+++ src/components/example/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app height="40" class="">
+  <v-footer app height="30" id="nav" class="position-fixed position-fixed">
     <div
       class="text-caption text-disabled"
     >
@@ -61,12 +61,11 @@
   // ]
 </script>
 
-<style scoped lang="sass">
-  .social-link :deep(.v-icon)
-    color: rgba(var(--v-theme-on-background), var(--v-disabled-opacity))
-    text-decoration: none
-    transition: .2s ease-in-out
-
-    &:hover
-      color: rgba(25, 118, 210, 1)
+<style scoped>
+    #nav {
+    z-index: 999;
+    backdrop-filter: blur(20px);
+    position: fixed;
+    background: rgba(255, 255, 255, 0.9);
+    }
 </style>
