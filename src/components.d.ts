2ac77b6736d96e067abbce4b57c329ced6390bff
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-13 23:12:09 +0700

    Updated components, genererated

--- src/components.d.ts
+++ src/components.d.ts
@@ -7,10 +7,15 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
-    AppFooter: typeof import('./components/AppFooter.vue')['default']
-    HelloWorld: typeof import('./components/HelloWorld.vue')['default']
-    LandingPageHeader: typeof import('./components/LandingPageHeader.vue')['default']
+    AppFooter: typeof import('./components/example/AppFooter.vue')['default']
+    AuthDialog: typeof import('./components/AuthDialog.vue')['default']
+    AuthDialogue: typeof import('./components/AuthDialog.vue')['default']
+    HelloWorld: typeof import('./components/example/HelloWorld.vue')['default']
+    LandingPageHeader: typeof import('./pages/index.vue')['default']
+    P5Canvas: typeof import('./components/P5Canvas.vue')['default']
+    Preloader: typeof import('./components/Preloader.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
+    Sketch: typeof import('./components/Sketch.vue')['default']
   }
 }
