b88479e224e15e820b5692441984a50e13f3be81
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 04:53:45 +0700

    Updated Home Header with NavBar

---
+++ src/components/HomeHeader.vue
@@ -0,0 +1,65 @@
+<script setup>
+import Logo from '../assets/common/logo.svg'
+import Cross from '../assets/common/cross.svg'
+import Heart from '../assets/common/heart.svg'
+</script>
+<template>
+  <v-toolbar
+    class="py-2"
+  >
+    <v-toolbar-title
+    class="mx-0"
+    >
+      <router-link to="/" tag="span" style="cursor: pointer">
+        <Logo class="mr-1"/>
+      </router-link>
+      <Cross class="mr-1"/>
+      <Heart class=""/>
+    </v-toolbar-title>
+    <v-toolbar-items class="hidden-xs-only">
+      <v-btn
+        flat
+        v-for="item in menuItems"
+        :key="item.title"
+        :to="item.path"
+      >
+        <v-icon left dark>{{ item.icon }}</v-icon>
+        {{ item.title }}
+      </v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+
+<script>
+import AppFooter from '@/components/example/AppFooter.vue'
+
+export default {
+  name: 'App',
+  components: { AppFooter },
+  data () {
+    return {
+      sidebar: false,
+      menuItems: [
+        {
+          title: 'Home',
+          path: '/home',
+          icon: 'home'
+        },
+        {
+          title: 'Sign Up',
+          path: '/signup',
+          icon: 'face'
+        },
+        {
+          title: 'Sign In',
+          path: '/signin',
+          icon: 'lock_open'
+        }
+      ]
+    }
+  },
+}
+</script>
+<style>
+
+</style>
