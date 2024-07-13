38e73f40d9d022ebcb9a4b7542f90434de223474
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-13 23:12:43 +0700

    added landing page to router

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,8 @@ import { createRouter, createWebHistory } from 'vue-router/auto'
 import { setupLayouts } from 'virtual:generated-layouts'
 import { routes } from 'vue-router/auto-routes'
 
+// import {LandingPage} from '@/pages/LandingPage.vue'
+
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: setupLayouts(routes),
