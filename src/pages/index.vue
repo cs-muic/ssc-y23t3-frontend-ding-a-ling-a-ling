985ed8334859e4fc5633096d17855eb05b658b0b
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 04:58:33 +0700

    Updated index with the new components

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,14 @@
 <template>
-  <div>
-    <v-row>
-      <v-col>
-        <h1>Welcome to Our Site!</h1>
-        <p>This is the landing page.</p>
-        <P5Canvas />
-      </v-col>
-    </v-row>
+  <div class="mx-5">
+    <HomeHeader/>
+    <OnboardCard/>
+    <AppFooter/>
   </div>
-  <!-- <AppFooter /> -->
 </template>
 
 <script lang="ts">
 </script>
 
 <style scoped>
-h1 {
-  text-align: center;
-}
+
 </style>
