3b6626a92f8226906936b3414da677b08aab2cda
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 07:02:53 +0700

    Place holder cards for landing page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="mx-5">
     <HomeHeader/>
+    <hr class="mb-10"/>
+    <hr class="mb-10"/>
+    <OnboardCard/>
+    TODO: Add a loop to display onboard cards
+    <OnboardCard/>
+    <OnboardCard/>
     <OnboardCard/>
     <AppFooter/>
   </div>
