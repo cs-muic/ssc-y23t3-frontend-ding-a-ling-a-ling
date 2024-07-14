f22cc5971accb169bea1ca4163dce97d8661a5eb
Austin Maddison <Austin.madison10152@gmail.com>
2024-07-14 07:00:48 +0700

    Imported SVGs to Preloader for anim

--- src/components/Preloader.vue
+++ src/components/Preloader.vue
@@ -1,3 +1,9 @@
+<script setup>
+import Logo from '../assets/common/logo.svg'
+import Cross from '../assets/common/cross.svg'
+import Heart from '../assets/common/heart.svg'
+</script>
+
 <template>
   <div class="preloader">
     <v-progress-circular color="primary" indeterminate />
